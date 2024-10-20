f5fbd5221df376b90ca61c1e0e1f8cfd2551ab47
João Vitor <joaovitorso071@gmail.com>
2024-10-20 14:40:02 -0300

    Aprendendo a colocar itens no minecraft

--- src/main/java/john/mod/PrimeiroMod.java
+++ src/main/java/john/mod/PrimeiroMod.java
@@ -1,5 +1,6 @@
 package john.mod;
 
+import john.mod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class PrimeiroMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
