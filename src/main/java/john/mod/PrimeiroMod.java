d77f91d05c1596d04c8f1371292c4d0126a6c18a
João Vitor <joaovitorso071@gmail.com>
2024-10-20 17:39:28 -0300

    Parte 3 do tutorial - Adicao de blocos

--- src/main/java/john/mod/PrimeiroMod.java
+++ src/main/java/john/mod/PrimeiroMod.java
@@ -1,5 +1,6 @@
 package john.mod;
 
+import john.mod.block.ModBlocks;
 import john.mod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,5 +14,6 @@ public class PrimeiroMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
